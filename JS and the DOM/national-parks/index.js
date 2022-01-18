e8e46b7bc0cb49367b4a8e800f7da8fcf598d0c8
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-01-18 00:06:23 -0700

    National Parks

---
+++ JS and the DOM/national-parks/index.js
@@ -0,0 +1,9 @@
+const allBtns = document.querySelectorAll(".rate-button");
+
+// Iterate through the list of buttons and add an event handler to each
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const park = event.target.parentNode;
+    park.style.backgroundColor = "#c8e6c9";
+  });
+});

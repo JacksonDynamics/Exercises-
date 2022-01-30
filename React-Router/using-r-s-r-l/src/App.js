fd7e3d5a26714b329b898156fd7544ac770fd4f7
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-01-30 16:52:10 -0700

    React Router V6 updates

---
+++ React-Router/using-r-s-r-l/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from "react-router-dom";
+
+
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
+
+function App() {
+  
+  return (
+    <Router>
+      
+      <Link to='/'>Home</Link>
+      <br/>
+      <Link to='/about'>About</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+    />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

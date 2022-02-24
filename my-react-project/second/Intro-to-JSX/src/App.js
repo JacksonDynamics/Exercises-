e615a56877b56226c09c18542dbba9bdb0e5d893
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-02-24 16:47:22 -0700

    Lesson

---
+++ my-react-project/second/Intro-to-JSX/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  const name = "Alice";
+  const url = "https://www.thinkful.com/";
+  const linkText = "Thinkful";
+  const element = (
+    <p>
+      Hey, {name}! Please visit <a href={url}>{linkText}</a>.
+    </p>
+  );
+  
+  return element;
+}
+
+export default App;

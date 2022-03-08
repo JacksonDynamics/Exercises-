274cbda69ee1c5891005640143e44d625e9a0feb
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-03-07 20:11:03 -0700

    GroceryList update

--- my-react-project/second/lists-and-tables/src/GroceryList.jsx
+++ my-react-project/second/lists-and-tables/src/GroceryList.jsx
@@ -1,30 +1,26 @@
-const groceryList = [
-    { quantity: 6, item: "Bananas" },
-    { quantity: 3, item: "Apples" },
-    { quantity: 10, item: "Oranges" },
-  ];
+
   
 
 function GroceryList({ items }) {
-    const rows = groceryList.map(({ quantity, item }, index) => (
-      <tr key={index}>
-        <td>{quantity}</td>
-        <td>{item}</td>
-      </tr>
-    ));
-  
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Quantity</th>
-            <th>Item</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    );
-  }
+    const rows = items.map(({ completed, description }, index) => (
+        <tr key={index}>
+          <td>{description}</td>
+          <td>{completed ? "Yes" : "No"}</td>
+        </tr>
+      ));
+    
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Completed?</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      );
+    }
   
 
   export default GroceryList

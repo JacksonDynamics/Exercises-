5a987796dc0735e1bb54962d9be36bc830f17f3e
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-03-09 19:02:55 -0700

    src

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import WelcomeBack from "./WelcomeBack";
+import Quote from "./Quote";
+
+const quote = {
+  text:
+    "I am a great believer in luck, and I find the harder I work, the more I have of it.",
+  author: "Thomas Jefferson",
+};
+
+function App() {
+  return (
+    <>
+      <WelcomeBack name="Joe" />
+      <Quote quote={quote} />
+    </>
+  );
+}
+
+export default App;

a6aae3dbba7736d6ed74ced200caa07f729f7f56
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-03-26 02:21:21 -0600

    Lesson

---
+++ my-react-project/second/React-Router/Route-Parameters/src/App.js
@@ -0,0 +1,37 @@
+
+import React from "react";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import UserProfile from "./UserProfile";
+import NoMatch from "./NoMatch"
+
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        {Array(10)
+          .fill()
+          .map((ignoredValue, index) => index + 1)
+          .map((id) => (
+            <div key={id}>
+              <Link to={`/user/${id}`}>User {id}</Link>
+            </div>
+          ))}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user/:userId" element={<UserProfile/>} />
+          <Route  path="*" element={<NoMatch />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

4c50f850cdc3b5ed439a0532c43f24fd18c02ea2
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-03-31 18:53:03 -0600

    Lesson

---
+++ Front-End-Deployment/Vercel/starter-constellations-client/build/precache-manifest.8b861c00aedaca3995a5dc93a5332736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607b8abdcec1075205c8d36ef4e43732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5624c4b9f2c55de93937",
+    "url": "/static/js/2.3e43d476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e43d476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517a5b0b1982ae1fe2d6",
+    "url": "/static/js/main.55386ebe.chunk.js"
+  },
+  {
+    "revision": "7d9b45b3c7c21bc99a53",
+    "url": "/static/js/runtime-main.1179b1de.js"
+  }
+]);

6ac530ac2b246a75bcc43104eeb26b0833ecda06
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-03-31 20:27:35 -0600

    Lesson 2

---
+++ Front-End-Deployment/Vercel/starter-constellations-client/build/precache-manifest.3ceab34ced2ba05845f3791132734977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde31a5ce05856a7fe6c470049945064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5624c4b9f2c55de93937",
+    "url": "/static/js/2.3e43d476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e43d476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aacf314e8861e45a0b93",
+    "url": "/static/js/main.94e2f048.chunk.js"
+  },
+  {
+    "revision": "7d9b45b3c7c21bc99a53",
+    "url": "/static/js/runtime-main.1179b1de.js"
+  }
+]);
--- Front-End-Deployment/Vercel/starter-constellations-client/build/service-worker.js
+++ Front-End-Deployment/Vercel/starter-constellations-client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8b861c00aedaca3995a5dc93a5332736.js"
+  "/precache-manifest.3ceab34ced2ba05845f3791132734977.js"
 );
 
 self.addEventListener('message', (event) => {

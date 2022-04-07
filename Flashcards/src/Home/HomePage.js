2c66b5b049b0a04ff3ae50f9b508e5f4fc770831
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-04-06 20:05:00 -0600

    Flashcards

--- Flashcards/src/Home/HomePage.js
+++ Flashcards/src/Home/HomePage.js
@@ -1,47 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import DeckItem from "../DeckList/DeckItem";
-import { listDecks } from "../utils/api";
+import React, {useState} from "react"
+import DeckList from "../Deck/DeckList"
 
-function HomePage() {
-  const [decks, setDecks] = useState([]);
-
-  // await listDecks API call to get decks then
-  // set decks state as the response
-  useEffect(() => {
-    const getData = async () => {
-      const response = await listDecks();
-      setDecks(response);
-    };
-    getData();
-  }, []);
-
-  // function to delete deck by filtering state decks by id
-  // to be passed down to DeckItem components
-  const onClickDelete = (id) => {
-    const newDecks = decks.filter((deck) => deck.id !== Number(id));
-    setDecks(newDecks);
-  };
-
-  return (
-    <div>
-      <div className="actions pb-4">
-        <Link to="/decks/new">
-          <button className="btn btn-secondary mb-2">
-          <span className="oi oi-plus" /> Create Deck</button>
-        </Link>
-      </div>
-      <ul className="list-group my-2">
-        {decks.map((deck) => (
-          <DeckItem
-            key={deck.id}
-            deck={deck}
-            onClickDelete={() => onClickDelete(deck.id)}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+function Home () {
+    const [decks, setDecks] = useState([])
+    
+    if (decks) {
+      return (
+        <>
+          <DeckList decks={decks} setDecks={setDecks} />
+        </>
+      )
+    } else {
+      return <h1> Loading... </h1>
+    }    
 }
 
-export default HomePage;
+export default Home

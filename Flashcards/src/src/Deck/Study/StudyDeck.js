023c8836cad298ea7ed6306b9110aefe774c5a8f
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-04-08 14:03:53 -0600

    Updated Study Cards Flips

---
+++ Flashcards/src/src/Deck/Study/StudyDeck.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { readDeck } from "../../utils/api";
+import { useParams, Link } from "react-router-dom";
+import StudyView from "./StudyCardView"
+
+function StudyDeck() {
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState({});
+
+  useEffect(() => {
+    readDeck(deckId).then(setDeck);
+  }, [deckId]);
+
+   
+    return (
+      <>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/">
+                <span className="oi oi-home" /> Home
+              </Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to={`/decks/${deckId}`}> {deck.name}</Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              {" "}
+              Study
+            </li>
+          </ol>
+        </nav>
+        <h1>{deck.name}: Study</h1>
+
+
+      {(() => {
+        if (deck.cards && deck.cards.length > 2) {
+          return (
+            <StudyView />
+          )
+        } else {
+          return (
+            <>
+                <h3>Not enough cards.</h3>
+                <p>You need at least 3 cards to study. There are cards in this deck</p>
+            </>
+          )
+        }
+      })()}
+      <br/>
+      <Link to={`/decks/${deckId}/cards/new`} className="btn btn-primary mb-2">
+                <span className="oi oi-plus" /> Add Cards </Link>
+    </>
+  )
+        
+}
+
+export default StudyDeck;
+
+

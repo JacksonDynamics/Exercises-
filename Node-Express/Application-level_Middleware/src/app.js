558f066d0f004ec407ce7baee53c99980f83a78c
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-04-13 22:13:49 -0600

    Add files via upload

--- Node-Express/Application-level_Middleware/src/app.js
+++ Node-Express/Application-level_Middleware/src/app.js
@@ -1,3 +1,13 @@
-const express = require("express")
-const app = express()
-module.exports = app
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("dev"));
+
+const sayHello = (req, res, next) => {
+  res.send("Hello!");
+};
+
+app.use(sayHello);
+
+module.exports = app;

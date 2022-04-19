6c0f38e85f1437e51933d4cd7a2159d1e315a055
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-04-19 15:50:49 -0600

    Add files via upload

--- Node-Express/Building-Routes/src/server.js
+++ Node-Express/Building-Routes/src/server.js
@@ -1,3 +1,3 @@
 const { PORT = 5000 } = process.env
 const app = require("./app")
-app.listen(PORT, () => console.log(`Listen on Port ${PORT}!`))
+app.listen(PORT, () => console.log(`Listening on Port ${PORT}!`))

5a16e3a28424bf072abdfe8b6ae996fadc819ae4
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-04-19 18:42:14 -0600

    Parameters

--- Node-Express/Query&Route_Params/src/app.js
+++ Node-Express/Query&Route_Params/src/app.js
@@ -4,11 +4,27 @@ const app = express();
 
 app.use(morgan("dev"));
 
-const sayHello = (req, res, next) => {
-    res.send("Hello!")
-}
+const sayHello = (req, res) => {
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}!` : "Hello!";
+    res.send(content);
+};
+
+const saySomething = (req, res) => {
+    const greeting = req.params.greeting;
+    const name = req.query.name;
+    const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+    res.send(content);
+};
+
+const sayGoodbye = (req, res) => {
+    res.send("Sorry to see you go!");
+};
+  
 
 app.get("/hello", sayHello)
+app.get("/say/goodbye", sayGoodbye);
+app.get("/say/:greeting", saySomething);
 
 
 
